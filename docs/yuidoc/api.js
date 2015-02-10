275aca6e890aa15cbd5822c25f825626920a1b43
Timo Kissing <timo.kissing@citrix.com>
2015-02-10 10:51:22 -0800

    Add generated docs to source control

---
+++ docs/yuidoc/api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Sample_JSDoc",
+        "Sample_YUIDoc",
+        null
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
